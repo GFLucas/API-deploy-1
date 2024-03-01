773fd3ccc42b55fbfdc0515685d10b3fda57b6b7
Lucas Guilherme <lucassgfcosta@gmail.com>
2024-03-01 11:13:27 -0400

    teste

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 require('dotenv').config()
 const express = require('express')
+const knex = require('./conexao')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', async (req, res) => {
-	return res.json('Api está Ok!')
+	try {
+		const carros = await knex('carros')
+		return res.json(carros)
+
+	} catch (error) {
+		console.log(error)
+		return res.status(500).json({ MENSAGEM: 'Erro do servidor' })
+	}
 })
 
 const port = process.env.PORT || 3000
